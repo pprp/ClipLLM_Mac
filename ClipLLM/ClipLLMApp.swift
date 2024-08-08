78af0f9988ae8e78da1cb9e15c19573f1d329aaf
pprp <dongpeijie98@gmail.com>
2024-08-08 20:59:09 +0800

    Initial Commit

---
+++ ClipLLM/ClipLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipLLMApp.swift
+//  ClipLLM
+//
+//  Created by peyton on 8/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClipLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
